214d589d89ec926236a667ead479933dd6de683d
eriksonoliveira <eriksonmagno6@hotmail.com>
2025-11-09 18:19:36 -0500

    Use font through next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "App Template",
 };
+
+const rubik = Rubik({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-rubik",
+});
 
 export default function RootLayout({
   children,
@@ -12,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${rubik.className}`}>{children}</body>
     </html>
   );
 }
